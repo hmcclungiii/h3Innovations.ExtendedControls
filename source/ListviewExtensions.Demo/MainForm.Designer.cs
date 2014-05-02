59e31724088802eed277b9542796efbef28a6be9
Houston McClung <hmcclungiii@gmail.com>
2014-05-02 13:59:44 -0400

    Demo application is working. First beta release, uploading to GitHub

--- source/ListviewExtensions.Demo/MainForm.Designer.cs
+++ source/ListviewExtensions.Demo/MainForm.Designer.cs
@@ -30,27 +30,118 @@ namespace ListviewExtensions.Demo
 		private void InitializeComponent()
 		{
 			this.listview1 = new h3Innovations.Listview();
+			this.columnHeader1 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader2 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader3 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader4 = new System.Windows.Forms.ColumnHeader();
+			this.columnHeader5 = new System.Windows.Forms.ColumnHeader();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.button4 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// listview1
 			// 
-			this.listview1.Location = new System.Drawing.Point(8, 8);
+			this.listview1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+									this.columnHeader1,
+									this.columnHeader2,
+									this.columnHeader3,
+									this.columnHeader4,
+									this.columnHeader5});
+			this.listview1.Location = new System.Drawing.Point(8, 152);
 			this.listview1.Name = "listview1";
-			this.listview1.Size = new System.Drawing.Size(264, 168);
+			this.listview1.Size = new System.Drawing.Size(504, 40);
 			this.listview1.TabIndex = 0;
 			this.listview1.UseCompatibleStateImageBehavior = false;
 			this.listview1.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeader1
+			// 
+			this.columnHeader1.Text = "My Column";
+			// 
+			// columnHeader2
+			// 
+			this.columnHeader2.Text = "Second Col";
+			// 
+			// columnHeader3
+			// 
+			this.columnHeader3.Text = "Another one";
+			// 
+			// columnHeader4
+			// 
+			this.columnHeader4.Text = "fourth column";
+			// 
+			// columnHeader5
+			// 
+			this.columnHeader5.Text = "one last one";
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.IsWebBrowserContextMenuEnabled = false;
+			this.webBrowser1.Location = new System.Drawing.Point(8, 8);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.ScriptErrorsSuppressed = true;
+			this.webBrowser1.ScrollBarsEnabled = false;
+			this.webBrowser1.Size = new System.Drawing.Size(504, 136);
+			this.webBrowser1.TabIndex = 1;
+			this.webBrowser1.Url = new System.Uri("res://ListviewExtensions.Demo.exe/help.htm", System.UriKind.Absolute);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(8, 200);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 4;
+			this.button2.Text = "Save";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.Button2Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(96, 200);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 5;
+			this.button3.Text = "Load";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.Button3Click);
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(440, 200);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(75, 23);
+			this.button4.TabIndex = 6;
+			this.button4.Text = "Close";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.Button4Click);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(284, 264);
+			this.ClientSize = new System.Drawing.Size(522, 229);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.webBrowser1);
 			this.Controls.Add(this.listview1);
 			this.Name = "MainForm";
 			this.Text = "ListviewExtensions.Demo";
+			this.Load += new System.EventHandler(this.MainFormLoad);
 			this.ResumeLayout(false);
 		}
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.ColumnHeader columnHeader5;
+		private System.Windows.Forms.ColumnHeader columnHeader4;
+		private System.Windows.Forms.ColumnHeader columnHeader3;
+		private System.Windows.Forms.ColumnHeader columnHeader2;
+		private System.Windows.Forms.ColumnHeader columnHeader1;
 		private h3Innovations.Listview listview1;
 	}
 }
--- source/ListviewExtensions.Demo/MainForm.cs
+++ source/ListviewExtensions.Demo/MainForm.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
 
 namespace ListviewExtensions.Demo
 {
@@ -11,6 +13,28 @@ namespace ListviewExtensions.Demo
 		public MainForm()
 		{
 			InitializeComponent();
+		}
+		
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			//webBrowser1.Navigate("res://ListviewExtensions.Demo.exe/help.htm");
+			Stream docStream =Assembly.GetExecutingAssembly().GetManifestResourceStream("help.htm");
+			webBrowser1.DocumentStream = docStream;
+		}
+		
+		void Button4Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+		
+		void Button2Click(object sender, EventArgs e)
+		{
+			listview1.SaveColumns();
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			listview1.LoadColumns();
 		}
 	}
 }
